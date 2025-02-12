d5c5543ca6e043b67cbc8521abd617df29924527
Seabass <luhan.extra@gmail.com>
2025-02-12 18:19:30 +0800

    feat: config database orm

---
+++ src/db/schema.ts
@@ -0,0 +1,13 @@
+import { integer, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid('id').primaryKey().defaultRandom(),
+  clerkId: text('clerk_id').unique().notNull(),
+  name: text('name').notNull(),
+  // TODO: add banner field
+
+  imageUrl: text('image_url').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  email: text('email').notNull().unique(),
+});
