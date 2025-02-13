483d43cf83dade53d196b25da0b6c99a36fd9d91
Seabass <luhan.extra@gmail.com>
2025-02-13 18:23:23 +0800

    feat: add webhooks route

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,13 +1,12 @@
 import { integer, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const users = pgTable("users", {
   id: uuid('id').primaryKey().defaultRandom(),
   clerkId: text('clerk_id').unique().notNull(),
   name: text('name').notNull(),
   // TODO: add banner field
 
   imageUrl: text('image_url').notNull(),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at').notNull().defaultNow(),
-  email: text('email').notNull().unique(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
